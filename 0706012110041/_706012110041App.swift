1aa9fc4976469bea7863db9bbd5e00e0bc4c615d
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-11 08:18:29 +0700

    Initial Commit

---
+++ 0706012110041/_706012110041App.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110041App.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110041App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
