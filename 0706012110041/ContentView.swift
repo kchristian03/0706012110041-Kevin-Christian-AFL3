4d2ea1b9996a894e52b64cbecda002b0ad58a262
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-11 08:37:01 +0700

    Section 3

--- 0706012110041/ContentView.swift
+++ 0706012110041/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Kevin!")
+        VStack (alignment: .leading){
+            Text("Turtle Rock")
                 .font(.title)
-                .foregroundColor(.green)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
