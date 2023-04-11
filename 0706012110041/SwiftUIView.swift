44e87574874d8debe111682ceb6d23f986cd1013
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-11 08:47:26 +0700

    Section 4

---
+++ 0706012110041/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 11/04/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Image("turtle")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
