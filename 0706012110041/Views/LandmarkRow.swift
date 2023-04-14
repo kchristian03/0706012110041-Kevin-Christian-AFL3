6590805fc06b198002da21f8298ade4fde73706c
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-14 09:26:10 +0700

    Section 2 Create the Row View

---
+++ 0706012110041/Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 14/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+            .previewLayout(.fixed(width: 300, height: 70))
+    }
+}
