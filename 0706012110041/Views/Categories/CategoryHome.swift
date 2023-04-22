aba8c8e4d4a54cab6e4a98fc843070fbacce90c8
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-22 13:16:03 +0700

    Section 1 - Add a Category View

---
+++ 0706012110041/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
