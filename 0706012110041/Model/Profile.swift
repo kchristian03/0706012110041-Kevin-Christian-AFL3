5f452e719c111d4ee6c7992f1cd1b86ca9770eb0
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-04-22 13:36:50 +0700

    Section 1 - Display a User Profile

---
+++ 0706012110041/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "user")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
---
+++ 0706012110041/Views/Profiles.swift
@@ -0,0 +1,20 @@
+//
+//  Profiles.swift
+//  0706012110041
+//
+//  Created by MacBook Pro on 22/04/23.
+//
+
+import SwiftUI
+
+struct Profiles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Profiles_Previews: PreviewProvider {
+    static var previews: some View {
+        Profiles()
+    }
+}
